02128ef6fe98f43e345cf2ab299fe4321a93b877
Prasoon Rai <arenodev.python@gmail.com>
2025-11-11 14:40:59 +0530

    Updated the main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,16 @@
 #include "raymath.h"
 #include "LightPoint.h++"
 
+const int ring = 50;
+const int slices = 50;
+
 int main()
 {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 1200;
+    const int screenHeight = 800;
 
     SetConfigFlags(FLAG_MSAA_4X_HINT);
-    InitWindow(screenWidth, screenHeight, "raylib [shaders] example - basic lighting");
+    InitWindow(screenWidth, screenHeight, "basic lighting in raylib");
 
     Camera camera = { 0 };
     camera.position = (Vector3){ 2.0f, 4.0f, 6.0f };
@@ -18,8 +21,8 @@ int main()
     camera.projection = CAMERA_PERSPECTIVE;
 
     // Load lighting shader
-    Shader shader = LoadShader(TextFormat("/Users/prasoonrai/Desktop/LearnRaylib/shaders/glsl%i/lighting.vs", GLSL_VERSION),
-                           TextFormat("/Users/prasoonrai/Desktop/LearnRaylib/shaders/glsl%i/lighting.fs", GLSL_VERSION));
+    Shader shader = LoadShader(TextFormat("shaders/glsl%i/lighting.vs", GLSL_VERSION),
+                           TextFormat("shaders/glsl%i/lighting.fs", GLSL_VERSION));
 
     // Create models and assign shader
     Model cube = LoadModelFromMesh(GenMeshCube(2.0f, 4.0f, 2.0f));
@@ -28,15 +31,16 @@ int main()
     Model plane = LoadModelFromMesh(GenMeshPlane(10.0f, 10.0f, 1, 1));
     plane.materials[0].shader = shader;
 
-    Model sphere = LoadModelFromMesh(GenMeshSphere(1.0f, 100, 100));
+    Model sphere = LoadModelFromMesh(GenMeshSphere(1.0f, ring, slices));
     sphere.materials[0].shader = shader;
 
     // Bake lights
     auto orange = Lights_Point((Vector3){ -2.0f, 1.0f, -2.0f }, ORANGE, true, "orange");
     auto red = Lights_Point((Vector3){ 2.0f, 1.0f, 2.0f }, RED, true, "red");
-    auto pink = Lights_Point((Vector3){ -2.0f, 1.0f, 2.0f }, BLUE, true, "pink");
+    auto pink = Lights_Point((Vector3){ -2.0f, 1.0f, 2.0f }, PINK, true, "pink");
+    auto blue = Lights_Point((Vector3){ 2.0f, 1.0f, -2.0f }, BLUE, true, "blue");
 
-    SetTargetFPS(1000);
+    SetTargetFPS(120);
 
     while (!WindowShouldClose())
     {
@@ -56,15 +60,20 @@ int main()
         }
         UpdateLightValues(shader, red.light);
 
-        if (IsKeyPressed(KEY_B)) {
+        if (IsKeyPressed(KEY_P)) {
             pink.light.enabled = !pink.light.enabled;
         }
         UpdateLightValues(shader, pink.light);
 
+        if (IsKeyPressed(KEY_B)) {
+            blue.light.enabled = !blue.light.enabled;
+        }
+        UpdateLightValues(shader, blue.light);
+
 
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+            ClearBackground(BLACK);
 
             BeginMode3D(camera);
 
@@ -72,14 +81,17 @@ int main()
                 DrawModel(sphere, Vector3(0.0f, 2.0f, 0.0f), 1.0f, WHITE);
 
                 // Draw sphere to show where the light is
-                if (orange.light.enabled) DrawSphereEx(orange.light.position, 0.2f, 8, 8, orange.light.color);
-                else DrawSphereWires(orange.light.position, 0.2f, 8, 8, ColorAlpha(orange.light.color, 0.3f));
+                if (orange.light.enabled) DrawSphereEx(orange.light.position, 0.2f, ring, slices, orange.light.color);
+                else DrawSphereWires(orange.light.position, 0.2f, ring, slices, ColorAlpha(orange.light.color, 0.3f));
 
-                if (red.light.enabled) DrawSphereEx(red.light.position, 0.2f, 8, 8, red.light.color);
-                else DrawSphereWires(red.light.position, 0.2f, 8, 8, ColorAlpha(red.light.color, 0.3f));
+                if (red.light.enabled) DrawSphereEx(red.light.position, 0.2f, ring, slices, red.light.color);
+                else DrawSphereWires(red.light.position, 0.2f, ring, slices, ColorAlpha(red.light.color, 0.3f));
 
-                if (pink.light.enabled) DrawSphereEx(pink.light.position, 0.2f, 8, 8, pink.light.color);
-                else DrawSphereWires(pink.light.position, 0.2f, 8, 8, ColorAlpha(pink.light.color, 0.3f));
+                if (pink.light.enabled) DrawSphereEx(pink.light.position, 0.2f, ring, slices, pink.light.color);
+                else DrawSphereWires(pink.light.position, 0.2f, ring, slices, ColorAlpha(pink.light.color, 0.3f));
+
+                if (blue.light.enabled) DrawSphereEx(blue.light.position, 0.2f, ring, slices, blue.light.color);
+                else DrawSphereWires(blue.light.position, 0.2f, ring, slices, ColorAlpha(blue.light.color, 0.3f));
 
                 DrawGrid(10, 1.0f);
 
